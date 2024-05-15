275610294f2246eda3dbe7411e205fe75a9c0120
Pazukha Vyacheslav <slava_pazukha@mail.ru>
2024-05-16 01:05:55 +0500

    Add heade.php

---
+++ assets/header.php
@@ -0,0 +1,5 @@
+<header class="header">
+  <a href="index.php" class="logo">Сайт-визитка</a>
+  <a href="vodorod.php" class="vodorod">Водород</a>
+  <a href="zadachi.php" class="zadachi">Задачи</a>
+</header>
