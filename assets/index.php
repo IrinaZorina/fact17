5c7c0eeba22ecfd343786cb944b58cb34d26d93d
Pazukha Vyacheslav <slava_pazukha@mail.ru>
2024-05-16 01:13:43 +0500

    Change

--- assets/index.php
+++ assets/index.php
@@ -1,15 +1,3 @@
-<?php
-$hour = date("H");
-
-$day = ($hour >= 8 && $hour < 20);
-
-if ($day) {
-  echo "<link rel='stylesheet' href=styles/style.css>"; 
-} else {
-  echo "<link rel='stylesheet' href=styles/style2.css>"; 
-}
- ?>
- 
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -19,86 +7,107 @@ if ($day) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-  <link rel="icon" href="media/foto.png">
+  <link rel="icon" href="media/vizitka.png">
+  </link>
+  <?php
+  date_default_timezone_set('Asia/Yekaterinburg'); //Выставил наш часовой пояс
+  $hour = date("H");
+
+  if ($hour >= 8 && $hour < 20) {
+    echo "<link rel='stylesheet' href='styles/vizitka.css'>";
+  } else {
+    echo "<link rel='stylesheet' href='styles/vizitka2.css'>";
+  }
+  ?>
 </head>
 
 <body>
 
-<header class="header">
-  <a href="index.php" class="logo">Сайт-визитка</a>
-  <a href="vodorod.php" class="vodorod">Водород</a>
-</header>
+  <?php
+  include 'header.php';
+  ?>
 
-<main>
-<div class="hero1">
-  <div class="container hero__container">
-    <div class="hero__box">
-      <img class="hero__box-img" src="media/me.png" alt="">
-    </div>
-    <div class="hero__about">
-      <h1 class="hero__title">Пазуха Вячеслав</h1>
-      <div class="container2">
-      <p class="hero__subtitle">
-        Я учусь в МГТУ в институте автоматизированных систем на направлении "Разработка игр и приложений".<br>
-        В университете я уже изучал html и css, но было это на первом курсе.<br>
-        Хочется в полном размере изучить данный курс и устроиться в компанию "Факт".</p>
-      <p class="hero__description">
-        Курс нравится мне тем, что в нём обучающимся во время лекции даётся задания
-        для самостоятельного выполнения, а позже эти задания разбираются с преподавателем.<br>
-      </p>
+  <main>
+    <div class="hero1">
+      <div class="container hero__container">
+        <div class="hero__box">
+          <img class="hero__box-img" src="media/me.png" alt="">
+        </div>
+        <div class="hero__about">
+          <h1 class="hero__title">Пазуха Вячеслав</h1>
+          <div class="container2">
+            <p class="hero__subtitle">
+              Я учусь в МГТУ в институте автоматизированных систем на направлении "Разработка игр и приложений".<br>
+              В университете я уже изучал html и css, но было это на первом курсе.<br>
+              Хочется в полном размере изучить данный курс и устроиться в компанию "Факт".</p>
+            <p class="hero__description">
+              Курс нравится мне тем, что в нём обучающимся во время лекции даётся задания
+              для самостоятельного выполнения, а позже эти задания разбираются с преподавателем.<br>
+            </p>
+          </div>
+          <div class="hero__links">
+            <a class="hero__link" href="https://vk.com/real_slavik" target="_blank"><img src="media/vk.png"></img></a>
+          </div>
+        </div>
       </div>
-      <div class="hero__links">
-        <a class="hero__link" href="https://vk.com/real_slavik" target="_blank"><img src="media/vk.png"></img></a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div>
+      <h1 class="car">Бренд авто</h1>
+    </div>
+    <div class="container3">
+      <div class="car1">
+        <div class="info1">BMW</div>
+        <div class="foto1"><img src="media/bmw.jpg" alt="bmw-logo"></div>
+      </div>
+      <div class="car2">
+        <div class="info2">MERCEDES</div>
+        <div class="foto2"><img src="media/mercedes.jpg" alt="mercedes-logo"></div>
+      </div>
+      <div class="car3">
+        <div class="info3">FORD</div>
+        <div class="foto3"><img src="media/ford.jpg" alt="ford-logo"></div>
+      </div>
+      <div class="car4">
+        <div class="info4">MITSUBISHI</div>
+        <div class="foto4"><img src="media/mitsu.jpg" alt="mitsubishi-logo"></div>
       </div>
     </div>
-  </div>
-</div>
-<br>
-<br>
-<br>
-<br>
-  <div><h1 class="car">Бренд авто</h1></div>
-  <div class="container3">
-    <div class="car1">
-      <div class="info1">BMW</div>
-      <div class="foto1"><img src="media/bmw.jpg" alt="bmw-logo"></div>
+    <div class="hero3">
+      <div>
+        <h1 class="odeshda">Бренд одежды</h1>
+      </div>
+      <div class="container4">
+        <div class="odeshda1">
+          <div class="info5">ADIDAS</div>
+          <div class="foto5"><img src="media/adidas.jpg" alt="adidas-logo"></div>
+        </div>
+        <div class="odeshda2">
+          <div class="info6">NIKE</div>
+          <div class="foto6"><img src="media/nike.jpg" alt="nike-logo"></div>
+        </div>
+        <br>
+        <div class="odeshda3">
+          <div class="info7">REEBOK</div>
+          <div class="foto7"><img src="media/reebok.jpg" alt="reebok-logo"></div>
+        </div>
+        <div class="odeshda4">
+          <div class="info8">PUMA</div>
+          <div class="foto8"><img src="media/puma.jpg" alt="puma-logo"></div>
+        </div>
+      </div>
     </div>
-    <div class="car2">
-      <div class="info2">MERCEDES</div>
-      <div class="foto2"><img src="media/mercedes.jpg" alt="mercedes-logo"></div>
-    </div>
-    <div class="car3">
-      <div class="info3">FORD</div>
-      <div class="foto3"><img src="media/ford.jpg" alt="ford-logo"></div>
-    </div>
-    <div class="car4">
-      <div class="info4">MITSUBISHI</div>
-      <div class="foto4"><img src="media/mitsu.jpg" alt="mitsubishi-logo"></div>
-    </div>
-  </div>
-<div class="hero3">
-<div><h1 class="odeshda">Бренд одежды</h1></div>
-<div class="container4">
-  <div class="odeshda1">
-    <div class="info5">ADIDAS</div>
-    <div class="foto5" ><img src="media/adidas.jpg" alt="adidas-logo"></div>
-  </div>
-  <div class="odeshda2">
-    <div class="info6">NIKE</div>
-    <div class="foto6"><img src="media/nike.jpg" alt="nike-logo"></div>
-  </div>
-  <br>
-  <div class="odeshda3">
-    <div class="info7">REEBOK</div>
-    <div class="foto7"><img src="media/reebok.jpg" alt="reebok-logo"></div>
-  </div>
-  <div class="odeshda4">
-    <div class="info8">PUMA</div>
-    <div class="foto8"><img src="media/puma.jpg" alt="puma-logo"></div>
-  </div>
-</div>
-</div>
-</main>
+    <br>
+    <?php
+    include 'footer.php';
+    ?>
+  </main>
 
 </body>
+
 </html>
--- assets/vodorod.php
+++ assets/vodorod.php
@@ -1,17 +1,6 @@
-<?php
-$hour = date("H");
-
-$day = ($hour >= 8 && $hour < 20);
-
-if ($day) {
-  echo "<link rel='stylesheet' href=styles/vodorod.css>"; 
-} else {
-  echo "<link rel='stylesheet' href=styles/vodorod2.css>"; 
-}
- ?>
-
 <!doctype html>
 <html lang="ru">
+
 <head>
     <title>Водород</title>
     <meta charset="utf-8">
@@ -19,21 +8,38 @@ if ($day) {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap">
     <link rel="stylesheet" href="styles/vodorod.css">
     <link rel="icon" href="media/vodo.png">
+    <?php
+    date_default_timezone_set('Asia/Yekaterinburg'); //Выставил наш часовой пояс
+    $hour = date("H");
+
+    if ($hour >= 8 && $hour < 20) {
+        echo "<link rel='stylesheet' href=styles/vodorod.css>";
+    } else {
+        echo "<link rel='stylesheet' href=styles/vodorod2.css>";
+    }
+    ?>
 </head>
+
 <body>
-<header class="header">
-    <a href="index.php" class="logo2">Сайт-визитка</a>
-    <a href="vodorod.php" class="vodorod2">Водород</a>
-</header>
-<main>
-    <div class="container">
-    <div class="cell2 s-element cell-main">
-        <span class="symbol"><strong>H</strong></span>
-        <span class="name"><strong>Водород</strong></span>
-        <span class="number"><strong>1</strong></span>
-        <span class="atomic-mass"><strong>1,00797</strong></span>
-    </div>
-    </div>
-</main>
+
+    <?php
+    include 'header.php';
+    ?>
+
+    <main>
+        <div class="container">
+            <div class="cell s-element cell-main">
+                <span class="symbol"><strong>H</strong></span>
+                <span class="name"><strong>Водород</strong></span>
+                <span class="number"><strong>1</strong></span>
+                <span class="atomic-mass"><strong>1,00797</strong></span>
+            </div>
+        </div>
+    </main>
+     <?php
+    include 'footer.php';
+    ?>
+
 </body>
+
 </html>
