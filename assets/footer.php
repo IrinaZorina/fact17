cad8e5af51b44652190671d967629dec12587434
Pazukha Vyacheslav <slava_pazukha@mail.ru>
2024-05-16 01:05:06 +0500

    Add footer.php

---
+++ assets/footer.php
@@ -0,0 +1,5 @@
+<header class="footer">
+    <a class="tg" href="https://t.me/real_slavik" target="_blank"><img src="media/tg.png"></img></a>
+    <a class="vk" href="https://vk.com/real_slavik" target="_blank"><img src="media/vk.png"></img></a>
+    <span class="foot">Веб-разработчик - Пазуха Вячеслав</span>
+</header>
