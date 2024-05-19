d7c57bf2722afd343d809889b83a43f50f657477
Reklanet <168105546+Reklanet@users.noreply.github.com>
2024-05-19 19:11:25 +0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <div class="footer">Copyright © 2024</div>
